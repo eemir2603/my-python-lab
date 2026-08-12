b04877f22038bfd9614941be4d6b8bab19d79002
El'Castillo🐧 <eemirutku59@gmail.com>
2026-08-12 04:19:05 +0300

    Add files via upload

---
+++ madlibs_game.py
@@ -0,0 +1,37 @@
+# Madlibs Game
+# This is a simple Madlibs game where you can add words and play the game by generating random sentences based on the words you provided.
+# Let's get started!
+
+
+place = input("Enter a place: ")
+animal = input("Enter an animal: ")
+action = input("Enter an action: ")
+object_ = input("Enter an object: ")
+adjective = input("Enter an adjective: ")
+person = input("Enter a person: ")
+another_action = input("Enter another action: ")
+fun_activity = input("Enter a fun activity: ")
+another_place = input("Enter another place: ")
+final_action = input("Enter a final action: ")
+
+print()
+print (" ---- Madlibs Story ---- ")
+print()
+
+
+
+
+
+print(f"Today I went to a {place}")
+print(f"and I saw a {animal}")
+print(f"who was {action}")
+print(f"with a {object_}")
+print(f"It was a {adjective} day!")
+print(f"Then I met a {person}")
+print(f"who was {another_action}")
+print(f"and we decided to {fun_activity} together.")
+print(f"After that, we went to a {another_place}")
+print(f"and we went to my appartment to {final_action}.")
+print(f"There, {person} decided to {final_action}.")
+print()
+print("Thanks for playing the Madlibs game!")
